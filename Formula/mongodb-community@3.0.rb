187fc996e198667dae03f099e72c59e3e617201a
Porter Schutz <porter@controlzee.com>
2019-09-26 16:18:10 -0700

    Take a crack at adding a mongo-community@3.0 formula

---
+++ Formula/mongodb-community@3.0.rb
@@ -0,0 +1,81 @@
+class MongodbCommunityAT32 < Formula
+  desc "High-performance, schema-free, document-oriented database"
+  homepage "https://www.mongodb.com/"
+
+  # frozen_string_literal: true
+
+  url "https://fastdl.mongodb.org/osx/mongodb-osx-ssl-x86_64-3.0.15.tgz"
+  sha256 "b0a7147e11ea3e14448222363a8277340ce157a3e104e0ae8fec62603fc25e68"
+
+  bottle :unneeded
+
+  keg_only :versioned_formula
+
+  def install
+    prefix.install Dir["*"]
+  end
+
+  def post_install
+    (var/"mongodb").mkpath
+    (var/"log/mongodb").mkpath
+    if !(File.exist?((etc/"mongod.conf"))) then
+      (etc/"mongod.conf").write mongodb_conf
+    end
+  end
+
+  def mongodb_conf; <<~EOS
+    systemLog:
+      destination: file
+      path: #{var}/log/mongodb/mongo.log
+      logAppend: true
+    storage:
+      dbPath: #{var}/mongodb
+    net:
+      bindIp: 127.0.0.1
+  EOS
+  end
+
+  plist_options :manual => "mongod --config #{HOMEBREW_PREFIX}/etc/mongod.conf"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/mongod</string>
+        <string>--config</string>
+        <string>#{etc}/mongod.conf</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <false/>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardErrorPath</key>
+      <string>#{var}/log/mongodb/output.log</string>
+      <key>StandardOutPath</key>
+      <string>#{var}/log/mongodb/output.log</string>
+      <key>HardResourceLimits</key>
+      <dict>
+        <key>NumberOfFiles</key>
+        <integer>4096</integer>
+      </dict>
+      <key>SoftResourceLimits</key>
+      <dict>
+        <key>NumberOfFiles</key>
+        <integer>4096</integer>
+      </dict>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system "#{bin}/mongod", "--sysinfo"
+  end
+end
